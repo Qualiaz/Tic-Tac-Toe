72a151c9cd12edd4c5f67f25fe23e28d79341e55
Qualiaz <qualiaz@protonmail>
2022-05-14 15:22:14 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic Tac Toe
+
+Small project application made as an assignment for theodinproject.com curriculum
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/app.js"),
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 5001,
+    open: true,
+    hot: false,
+    compress: true,
+  },
+
+  module: {
+    rules: [
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+
+      { test: /\.(svg|ico|png|webp|jpg|jpeg|gif)$/i, type: "asset/resource" },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "title",
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/template.html"), //create temp file
+    }),
+  ],
+};
